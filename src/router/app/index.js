5f17876454e7fd43e853b8969481e5076d53af72
capstone student <capstonestudent@pn.local>
2021-04-08 15:26:09 +0800

    Landing Page: How We Work

--- src/router/app/index.js
+++ src/router/app/index.js
@@ -1,9 +1,14 @@
 import Home from 'src/home/Landing.vue'
+import HowWeWork from 'src/home/Landing/HowWeWork.vue'
 export default{
   routes: [{
     path: '/',
     name: 'Home',
     component: Home
+  },{
+    path: '/howWeWork',
+    name: 'howWeWork',
+    component: HowWeWork
   }]
 }
 
