6f4e5223a5e211a3ba97cea4f944ca96dcfb4853
capstone student <capstonestudent@pn.local>
2021-04-12 16:54:39 +0800

    to be continue for footer and what we do

--- src/home/Landing.vue
+++ src/home/Landing.vue
@@ -11,7 +11,7 @@
     <LandingTechnologies />
     <LandingWhatTheySayAboutUs />
     <!-- Calendly inline widget begin -->
-    <div class="calendly-inline-widget" data-url="https://calendly.com/kennettecanales/30min" style="min-width:320px;height:630px;"></div>
+    <!--<div class="calendly-inline-widget" data-url="https://calendly.com/kennettecanales/30min" style="min-width:320px;height:630px;"></div>-->
     <!-- Calendly inline widget end -->
     <span class="return-to-top" @click="scrollTo()">
       <i class="fas fa-angle-up" style="font-size: 35px"></i>
