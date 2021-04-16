e842fa2077512b16eb7da279adf7df5c411b03fa
John_Louie Bersaluna <johnlouie.bersaluna@student.passerellesnumeriques.org>
2021-04-16 16:33:18 +0800

    Update on Projects

--- src/home/Landing/Projects.vue
+++ src/home/Landing/Projects.vue
@@ -1,70 +1,70 @@
 <template>
   <div class="cw-banner" id="projects">
-    <div class="text">
-      <div class="text-center mt-5 p-5">
-          <h1 class="entitle" ><b>Our Projects</b></h1>
-      </div>
-      <div class="container mt-5">
+    <h1 class="entitle" ><b>Our Projects</b></h1>
+      <div class="mt-5">
         <div class="col-sm-12 d-flex">
-          <div class="col-sm-3 m-0">
-              <div class="box row">
-                <img :src="require('assets/img/project - runway.png')">
-              </div>
-              <div class="description">
-                <span>Runway Express - Philippines</span>   
-                <p>Website and Mobile Development</p>                   
-              </div>
-              <a href="http://runwayexpress.co.uk/#/"><button class="button1">Visit Website</button></a>
-          </div>
-          <div class="col-sm-3 m-0">
+          <div class="col-sm-3" v-for="(item, i) in projects" :key="i" :index="i">
             <div class="row">
-              <img :src="require('assets/img/project - mezzo.png')">
+              <img :src="item.src">
             </div>
             <div class="description">
-              <span>Mezzo Hotel - Philippines</span>
-              <p>Website Development</p>
-            </div>      
-            <a href="https://mezzohotel.com/"><button class="button1">Visit Website</button></a>
-          </div>
-          <div class="col-sm-3 m-0">
-            <div class="row">
-              <img :src="require('assets/img/project - popoutposter.png')">
+              <span>{{item.title}}</span>
+              <p>{{item.description}}</p>
+              <a :href=item.link target="_blank"><button class="button1">Visit Website</button></a>      
             </div>
-            <div class="description">
-              <span>Popout Poster - United Kingdom</span>
-              <p>Website Development</p>
-            </div>
-            <a href="https://popoutposter.com/"><button class="button1">Visit Website</button></a>
-          </div>
-          <div class="col-sm-3 m-0">
-            <div class="row">
-              <img :src="require('assets/img/project - meetthesea.png')">
-            </div>
-            <div class="description">
-              <span>Meat The Sea - Hongkong</span>
-              <p>Website Development</p>
-            </div>
-            <a href="https://www.meatthesea.com/"><button class="button1">Visit Website</button></a>
           </div>
         </div>
       </div>
       <button class="button2">View More ></button>
-    </div>
   </div>
 </template>
+
+<script>
+import ROUTER from 'src/router'
+export default {
+  mounted(){
+  },
+  data(){
+    return {
+      projects: [{
+        title: 'Runway Express - Philippines',
+        description: 'Website and Mobile Development',
+        src: require("assets/img/project - runway.png"),
+        link: 'http://runwayexpress.co.uk/#/'
+      },{
+        title: 'Mezzo Hotel - Philippines',
+        description: 'Website Development',
+        src: require("assets/img/project - mezzo.png"),
+        link: 'https://mezzohotel.com/'
+      },{
+        title: 'Popout Poster - United Kingdom',
+        description: 'Website Development',
+        src: require("assets/img/project - popoutposter.png"),
+        link: 'https://popoutposter.com/'
+      },{
+        title: 'Meat The Sea - Hong Kong',
+        description: 'Website Development',
+        src: require("assets/img/project - meetthesea.png"),
+        link: 'https://www.meatthesea.com/'
+      }]
+    }
+  },
+  methods: {
+    redirect(parameter){
+      ROUTER.push(parameter)
+    }
+  }
+}
+</script>
 <style scoped lang="scss">
 @import "~assets/style/colors.scss";
-label{
-  font-size: 30px;
-  font-weight: bold;
-}
 .button1{
   background-color: #E0E0E0;
   border: none;
   color: #BDBDBD !important;
-  padding: 15px 32px;
+  padding: 1vw 2vw;
   text-align: center;
-  font-size: 13px;
+  font-size: 17px;
   cursor: pointer;
   border-radius: 10px;
   margin-left: 20%;
@@ -77,56 +77,50 @@ label{
   border-radius: 20px;
   border: none;
   color: white;
-  padding: 15px 32px;
+  padding: 15px 35px;
   text-align: center;
-  font-size: 20px;
+  font-size: 30px;
   cursor: pointer;
-  margin-left: 42%;
-  margin-right: 42%;
+  margin-left: 41%;
+  margin-right: 41%;
   margin-top: 10%;
 }
 .row{
-  width: 18.4vw;
+  width: 19vw;
   height: auto;
   border: 1px solid white;
   background-color: white;
+  margin-bottom: 5%;
 }
 .entitle {
   text-align: center;
   font-size: 55px;
-  margin-top: 3%;
+  margin-top: 5%;
+  color: white;
+  margin-bottom: 8%;
 }
 .cw-banner{
   width: 100%;
   float: left;
   background: $primary;
+  padding: 5%;
 }
 .description{
   text-align:center;
-  margin-left: -7%;
-  margin-top: 7%;
+  margin-left: -12%;
 }
 span{
   font-size: 17px;
+  color: white;
+  text-align: center;
 }
 p{
   font-size: 14px;
   color: #8F00B5;
-}
-.text{
-  color: white;
-  position: relative;
-  z-index: 1000;
-  margin-left: 5%;
-  margin-right: 5%;
-  padding-bottom: 10%;
-}
-.text .title{
-  width: 100%;
-  float: left;
+  text-align: center;
 }
 img{
-  width: 17vw;
+  width: 18vw;
   height: auto;
   margin:auto;
   padding: 5%;
@@ -148,24 +142,17 @@ img{
     max-height: 100%;
     width: inherit;
   }
-}
-
-</style>
-<script>
-import ROUTER from 'src/router'
-import COMMON from 'src/common.js'
-export default {
-  mounted(){
-  },
-  data(){
-    return {
-      common: COMMON
-    }
-  },
-  methods: {
-    redirect(parameter){
-      ROUTER.push(parameter)
-    }
+  span{
+    font-size: 13px;
   }
+  p{
+    font-size: 10px;
+  }
+  .button1{
+  font-size: 10px;
 }
-</script>
+  .button2{
+  font-size: 14px;
+}
+}
+</style>
