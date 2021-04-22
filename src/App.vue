6e4cb37b75441562b14ae606aad34b236f94b339
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-04-22 11:51:19 +0800

    Show header on all pages

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <!-- <Home/> -->
+    <LandingHeader></LandingHeader>
     <router-view></router-view>
+    <LandingFooter></LandingFooter>
   </div>
 </template>
 
 <script>
+import LandingHeader from "src/home/Landing/Header.vue";
+import LandingFooter from "src/home/Landing/Footer.vue";
 export default {
   components: {
-  }
-}
+    LandingHeader,
+    LandingFooter,
+  },
+};
 </script>
 <style lang="scss">
 @import "~assets/style/colors.scss";
 #app {
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
 }
 </style>
