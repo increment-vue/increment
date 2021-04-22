f7d57b513ac403d2759f5ec4e58aabd239b7b901
capstone student <capstonestudent@pn.local>
2021-04-22 15:00:18 +0800

    increment website updates

--- src/home/case-studies/RFIDSystem.vue
+++ src/home/case-studies/RFIDSystem.vue
@@ -1,121 +1,74 @@
 <template>
   <div>
-    <img src="../../assets/img/bg-rfid.jpg" />
-    <h1>RFID System</h1>
-    <h2>Starting Price: $7,000</h2>
-    <div class="container">
+    <div class="banner">
+      <h1 class="title">RFID SYSTEM</h1>
+      <h3 id="start-price">Starting Price : $7,000</h3>
       <a
-        href="https://calendly.com/incrementtech2020"
+        href="https://calendly.com/incrementtech2020/services-offered"
+        id="btn"
         type="button"
-        class="btn btn-success custom"
         >Inquire Now!</a
       >
     </div>
-    <p id="text">
-      RFID Systems are used in many industries and in a wide variety of
-      applications. Imagine yourself being able to immediately locate everything
-      you have misplaced or lost? This is what RFID technology can do. Most
-      businesses invest in an RFID system to increase their employees’
-      productivity wherein they don’t have to be within line of sight of an item
-      or asset to locate it. It is also one of the most effective solutions used
-      in business to organize systems by eliminating manual procedures, along
-      with increased inventory and asset visibility, and most especially it
-      enables the elimination of human error.
-    </p>
-    <p id="text1">
-      Increment Technologies, can take your business into a higher level of
-      management. We can definitely develop and create a RFID system exclusively
-      for your organization. Creating opportunities for your company to minimize
-      resources, human error, and increase your efficiency in the processes.
-      This means that we will design a centralized system, so that you can
-      streamline your processes.
-    </p>
-    <br />
-    <br />
-    <br />
+    <div class="container">
+      <p>
+        RFID Systems are used in many industries and in a wide variety of applications. Imagine yourself being able to immediately locate everything you have misplaced or lost? This is what RFID technology can do. Most businesses invest in an RFID system to increase their employees’ productivity wherein they don’t have to be within line of sight of an item or asset to locate it. It is also one of the most effective solutions used in business to organize systems by eliminating manual procedures, along with increased inventory and asset visibility, and most especially it enables the elimination of human error. 
+
+
+      </p>
+      <p>
+        Increment Technologies, can take your business into a higher level of management. We can definitely develop and create a RFID system exclusively for your organization. Creating opportunities for your company to minimize resources, human error, and increase your efficiency in the processes. This means that we will design a centralized system, so that you can streamline your processes.
+
+      </p>
+    </div>
   </div>
 </template>
+<script>
+</script>
 <style scoped lang="scss">
 @import "~assets/style/colors.scss";
-img {
-  height: 600px;
+.banner {
+  background-image: url("../../assets/img/bg-rfid.jpg");
+  background-size: cover;
+  height: 70vh;
   width: 100%;
-}
-#text {
-  margin-top: 130px;
-  margin-left: 115px;
-  margin-right: 115px;
-  font-size: 16px;
-}
-#text1 {
-  margin-top: 30px;
-  margin-left: 115px;
-  margin-right: 115px;
-  font-size: 16px;
+  text-align: center;
+  color: white;
+  padding: 35vh 0;
 }
 
-h1 {
-  color: white;
-  margin-top: 150px;
-  position: absolute;
-  left: 36%;
-  right: 36%;
-  top: 98px;
-  /* font-family: Open Sans; */
-  font-style: normal;
-  font-weight: bold;
+.title {
   font-size: 50px;
-  line-height: 68px;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-  text-align: center;
+  font-weight: 800;
+  padding-bottom: 10px;
 }
-h2 {
-  position: absolute;
-  width: 329px;
-  height: 34px;
-  left: 37%;
-  top: 320px;
-  /* margin-top: 115px; */
-  font-style: normal;
+
+#start-price {
   font-size: 30px;
-  line-height: 41px;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-  text-align: center;
-  color: #ffffff;
+  font-weight: 600;
+  padding-bottom: 10px;
 }
-.btn-success {
-  color: #fff;
+
+#btn {
   background-color: #00e68a;
+  color: white;
+  //background: #00E68A;
   box-shadow: 0px 5px 10px rgba(255, 255, 255, 0.25);
   border-radius: 10px;
-  outline: none;
-  border: 0px;
+  border: none;
+  //color: white;
+  padding: 10px 20px;
+  text-align: center;
+  cursor: pointer;
 }
-.btn {
-  display: inline-block;
-  font-weight: 400;
-  margin-top: 180px;
-  margin-left: 43%;
-}
+
 .container {
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
+  margin-top: 10vh;
+  margin-bottom: 30vh;
 }
-.custom:hover {
-  background-color: #00e68a !important;
+
+p {
+  text-align: justify;
+  margin-bottom: 30px;
 }
 </style>
