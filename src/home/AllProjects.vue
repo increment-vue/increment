b808f9716c7cb7ed206915382b5c0ea6a518fd8f
John_Louie Bersaluna <johnlouie.bersaluna@student.passerellesnumeriques.org>
2021-04-24 18:38:18 +0800

    Projects Responsive Update

--- src/home/AllProjects.vue
+++ src/home/AllProjects.vue
@@ -5,70 +5,20 @@
         </div>
         <br>
         <div class="cw-banner">
-            <div class="col-sm-12 d-flex">
-                <div class="col-sm-3" v-for="(item, i) in projects" :key="i" :index="i">
-                    <div class="row">
-                        <img :src="item.src" class="images">
-                    </div>
-                    <div class="description">
-                        <span>{{item.title}}</span>
-                        <p>{{item.description}}</p>
-                    </div>
-                    <a :href=item.link ><button v-show="item.withButton" v-if="item.withButton === true ? 'item.withButton':'!item.withButoon'" class="button1">Visit Website</button></a>
-                </div>
+            <div class="row no-gutters">
+        <div class="col-md-3" v-for="(item, i) in projects" :key="i" :index="i">
+          <center>
+            <div class="box">
+              <img class="images" :src="item.src">
             </div>
-            <br>
-            <div class="col-sm-12 d-flex">
-                <div class="col-sm-3" v-for="(item, i) in projects2" :key="i" :index="i">
-                    <div class="row">
-                        <img :src="item.src" class="images">
-                    </div>
-                    <div class="description">
-                        <span>{{item.title}}</span>
-                        <p>{{item.description}}</p>
-                    </div>
-                    <a :href=item.link ><button v-show="item.withButton" v-if="item.withButton === true ? 'item.withButton':'!item.withButoon'" class="button1">Visit Website</button></a>
-                </div>
+            <div class="description">
+              <span>{{item.title}}</span>
+              <p>{{item.description}}</p>
             </div>
-            <br>
-            <div class="col-sm-12 d-flex">
-                <div class="col-sm-3" v-for="(item, i) in projects3" :key="i" :index="i">
-                    <div class="row">
-                        <img :src="item.src" class="images">
-                    </div>
-                    <div class="description">
-                        <span>{{item.title}}</span>
-                        <p>{{item.description}}</p>
-                    </div>
-                    <a :href=item.link ><button v-show="item.withButton" v-if="item.withButton === true ? 'item.withButton':'!item.withButoon'" class="button1">Visit Website</button></a>
-                </div>
-            </div>
-            <br>
-            <div class="col-sm-12 d-flex">
-                <div class="col-sm-3" v-for="(item, i) in projects4" :key="i" :index="i">
-                    <div class="row">
-                        <img :src="item.src" class="images">
-                    </div>
-                    <div class="description">
-                        <span>{{item.title}}</span>
-                        <p>{{item.description}}</p>
-                    </div>
-                    <a :href=item.link ><button v-show="item.withButton" v-if="item.withButton === true ? 'item.withButton':'!item.withButoon'" class="button1">Visit Website</button></a>
-                </div>
-            </div>
-            <br>
-            <div class="col-sm-12 d-flex">
-                <div class="col-sm-3" v-for="(item, i) in projects5" :key="i" :index="i">
-                    <div class="row">
-                        <img :src="item.src" class="images">
-                    </div>
-                    <div class="description1">
-                        <span>{{item.title}}</span>
-                        <p>{{item.description}}</p>
-                    </div>
-                    <a :href=item.link ><button v-show="item.withButton" v-if="item.withButton === true ? 'item.withButton':'!item.withButoon'" class="button2">Visit Website</button></a>
-                </div>
-            </div>
+            <a :href=item.link target="_blank"><button class="button1">Visit Website</button></a>
+          </center>
+        </div>
+      </div>
         </div>
     </div>
 </template>
@@ -103,8 +53,8 @@ export default {
         src: require("assets/img/project - meetthesea.png"),
         link: 'https://www.meatthesea.com/',
         withButton: true
-      }],
-      projects2:[{
+      },
+      {
         title: 'Eat Study & Sip - United States',
         description: 'Website Development',
         src: require("assets/img/project - eatstudyandsip.png"),
@@ -128,8 +78,8 @@ export default {
         src: require("assets/img/project - pivot.jpg"),
         link: '',
         withButton: true
-      }],
-      projects3:[{
+      },
+      {
         title: 'Max Muscle - United States',
         description: 'Website Development',
         src: require("assets/img/project - maxmuscle.jpg"),
@@ -152,8 +102,8 @@ export default {
         src: require("assets/img/project - agricord.png"),
         link: 'http://traceag.com.au/#/',
         withButton: true
-      }],
-      projects4:[{
+      },
+      {
         title: 'AgriSend - Austrilia',
         description: 'Mobile Development',
         src: require("assets/img/project - agrisend.png"),
@@ -176,8 +126,8 @@ export default {
         src: require("assets/img/project - zetamariani.png"),
         link: 'http://zetamariani.com/',
         withButton: true
-      }],
-      projects5:[{
+      },
+      {
         title: 'DBSN Group - Philippines',
         description: 'Website Development',
         src: require("assets/img/project - dbsn.png"),
@@ -209,9 +159,6 @@ export default {
   color: white;
   padding: 30vh 0;
 }
-.d-flex{
-    text-align: center;
-}
 .entitle {
   font-size: 50px;
   font-weight: 800;
@@ -222,6 +169,7 @@ export default {
 .cw-banner{
   width: 100%;
   padding: 15%;
+  margin-top: -10%;
 }
 img{
     width: 100%;
@@ -232,7 +180,6 @@ img{
   border: none;
   color: white;
   padding: 1vw 2vw;
-  text-align: center;
   font-size: 15px;
   cursor: pointer;
   border-radius: 10px;
@@ -251,15 +198,17 @@ img{
   margin-top: 10%;
   outline-color: white;
 }
-.row{
-  width: 16vw;
+.box{
+  width: 90%;
   height: auto;
   border: 1px solid #00B89F ;
   background-color: white;
   margin-bottom: 5%;
-}   
+  margin-top: 30%;
+} 
 .description{
-  height: 12vh;
+  height: 9vh;
+  margin-bottom: 5%;
 }
 span{
   font-size: 15px;
@@ -271,436 +220,10 @@ p{
   color: #8F00B5;
 }
 .images{
-  width: 15vw;
+  width: 100%;
   height: auto;
   margin:auto;
   padding: 5%;
-}
-@media only screen and (max-width: 1033px){
-.entitle {
-  font-size: 50px;
-  font-weight: 800;
-  padding: 10px;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-  img{
-    width: 100%;
-    height: 500px;
-}
-  .description{
-    height: 15vh;
-  }
-  .description1{
-    height: 15vh;
-  }
-}
-@media only screen and (max-width: 1024px){
-  .entitle {
-  font-size: 50px;
-  font-weight: 800;
-  padding: 10px;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-  .description{
-    height: 15vh;
-  }
-  .description1{
-    height: 15vh;
-  }
-}
-@media only screen and (max-width: 992px){
-  .entitle {
-  font-size: 50px;
-  font-weight: 800;
-  padding: 10px;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-  span{
-    font-size: 10px;
-  }
-  p{
-    font-size: 10px;
-  }
-  .description{
-    height: 10vh;
-  }
-  .description1{
-    height: 10vh;
-  }
-  .button1{
-  font-size: 10px;
-}
-}
-@media only screen and (max-width: 800px){
-  .entitle {
-  font-size: 40px;
-  font-weight: 800;
-  padding: 10px;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-  img{
-    width: 100%;
-    height: 500px;
-}
-  .button1{
-    font-size: 9px
-  }
-  .button2{
-    font-size: 9px
-  }
-  .description{
-    height: 15vh;
-  }
-  .description1{
-    height: 15vh;
-  }
-    .images{
-  width: 13vw;
-  height: auto;
-}
-.row{
-  width: 15vw;
-  height: auto;
-}
-}
-@media only screen and (max-width: 690px){
-  img{
-    width: 100%;
-    height: 400px;
-}
-  .button1{
-    font-size: 8px
-  }
-    .button2{
-    font-size: 8px
-  }
-  .description{
-    height: 17vh;
-  }
-  .description1{
-    height: 15vh;
-  }
-    .images{
-  width: 13vw;
-  height: auto;
-}
-.row{
-  width: 15vw;
-  height: auto;
-}
-}
-@media only screen and (max-width: 640px){
-  img{
-    width: 100%;
-    height: 400px;
-}
-  .button1{
-    font-size: 6px
-  }
-    .button2{
-    font-size: 6px
-  }
-  .description{
-    height: 17vh;
-  }
-  .description1{
-    height: 15vh;
-  }
-  .images{
-  width: 13vw;
-}
-.row{
-  width: 15vw;
-  height: auto;
-}
-}
-@media only screen and (max-width: 600px){
-  img{
-    height: 400px;
-}
-  .description{
-    height: 10vh;
-  }
-  .description1{
-    height: 10vh;
-  }
-    .images{
-  width: 12vw;
-  height: auto;
-}
-.row{
-  width: 15vw;
-  height: auto;
-}
-}
-@media only screen and (max-width: 599px){
-  .entitle {
-  font-size: 40px;
-  font-weight: 800;
-  padding: 10px;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-  .button1{
-    font-size: 6px
-  }
-    .button2{
-    font-size: 6px
-  }
-  .description{
-    height: 18vh;
-  }
-  .description1{
-    height: 15vh;
-  }
-  .images{
-  width: 13vw;
-}
-.row{
-  width: 15vw;
-  height: auto;
-}
-.cw-banner{
-  width: 100%;
-  padding: 10%;
-}
-}
-@media only screen and (max-width: 540px){
-img{
-  width: 100%;
-  height: 400px;
-}
-  .button1{
-  font-size: 6px;
-}
-  .button2{
-  font-size: 6px;
-  margin-left: -83%;
-}
-  .description{
-    height: 20vh;
-  }
-  .description1{
-    height: 8vh;
-    margin-left: -83%;
-  }
-}
-@media only screen and (max-width: 425px){
-.cw-banner{
-  width: 100%;
-  padding: 5%;
-}
-
-}
-@media only screen and (max-width: 414px){
-  .cw-banner{
-  width: 100%;
-  padding: 10%;
-}
-.button1{
-  font-size: 5px;
-}
-img{
-  width: 100%;
-  height: 300px;
-}
-.row{
-  margin-bottom: 5%;
-} 
-.cw-banner{
-  width: 100%;
-  padding: 13%;
-  margin-top: 2%;
-}
-.images{
-  width: 13vw;
-  height: auto;
-  margin:auto;
-  padding: 5%;
-}
-span{
-  font-size: 5px;
-}
-p{
-  font-size: 3px;
-}
-.button1{
-  font-size: 2px;
-}
-.button2{
-  font-size: 2px;
-  margin-left: -86%;
-}
-.description{
-  height: 10vh;
-}
-.description1{
-  height: 3vh;
-  margin-left: -86%;
-}
-}
-@media only screen and (max-width: 411px){
-    .entitle {
-  font-size: 35px;
-  font-weight: 800;
-  padding: 10px;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-.button1{
-  font-size: 5px;
-}
-img{
-  width: 100%;
-  height: 300px;
-}
-.row{
-  margin-bottom: 5%;
-} 
-.cw-banner{
-  width: 100%;
-  padding: 13%;
-  margin-top: 2%;
-}
-.images{
-  width: 13vw;
-  height: auto;
-  margin:auto;
-  padding: 5%;
-}
-span{
-  font-size: 5px;
-}
-p{
-  font-size: 3px;
-}
-.button1{
-  font-size: 2px;
-}
-.button2{
-  font-size: 2px;
-  margin-left: -86%;
-}
-.description{
-  height: 10vh;
-}
-.description1{
-  height: 3vh;
-  margin-left: -86%;
-}
-}
-@media only screen and (max-width: 375px){
-    .entitle {
-  font-size: 30px;
-  font-weight: 800;
-  padding: 10px;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-.description{
-  height: 14vh;
-}
-.description1{
-  height: 3vh;
-  margin-left: -86%;
-}
-}
-@media only screen and (max-width: 360px){
-.button1{
-  font-size: 5px;
-}
-img{
-  width: 100%;
-  height: 300px;
-}
-.row{
-  margin-bottom: 5%;
-} 
-.cw-banner{
-  width: 100%;
-  padding: 13%;
-  margin-top: 2%;
-}
-.images{
-  width: 13vw;
-  height: auto;
-  margin:auto;
-  padding: 5%;
-}
-span{
-  font-size: 5px;
-}
-p{
-  font-size: 3px;
-}
-.button1{
-  font-size: 2px;
-}
-.button2{
-  font-size: 2px;
-  margin-left: -86%;
-}
-.description{
-  height: 15vh;
-}
-.description1{
-  height: 3vh;
-  margin-left: -86%;
-}
-}
-@media only screen and (max-width: 320px){
-    .entitle {
-  font-size: 20px;
-  font-weight: 800;
-  padding: 10px;
-  margin-right: 10%;
-  margin-left: 10%;
-}
-.button1{
-  font-size: 5px;
-}
-img{
-  width: 100%;
-  height: 300px;
-}
-.row{
-  margin-bottom: 5%;
-} 
-.cw-banner{
-  width: 100%;
-  padding: 13%;
-  margin-top: 2%;
-}
-.images{
-  width: 13vw;
-  height: auto;
-  margin:auto;
-  padding: 5%;
-}
-span{
-  font-size: 3px;
-}
-p{
-  font-size: 3px;
-}
-.button1{
-  font-size: 1px;
-}
-.button2{
-  font-size: 1px;
-  margin-left: -86%;
-}
-.description{
-  height: 15vh;
-}
-.description1{
-  height: 3vh;
-  margin-left: -86%;
-}
 }
 </style>
 
