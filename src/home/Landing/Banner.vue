01b3792f18edf709df8a5fdc2f73328866063d1e
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-05-06 10:18:41 +0800

    done banner

--- src/home/Landing/Banner.vue
+++ src/home/Landing/Banner.vue
@@ -6,11 +6,15 @@
           <b>WE WANT YOU TO SUCCEED</b>
         </h1>
         <h3>
-          We create beautiful and secured websites, and mobile applications.<br />
-          Just relax and we will give solutions to your needs. Got something<br />
+          We create beautiful and secured websites, and mobile applications.
+          Just relax and we will give solutions to your needs. Got something
           awesome for us to work on?
         </h3>
-        <a href="https://calendly.com/incrementtech2020/services-offered" target="_blank"><button>Contact Us</button></a>
+        <a
+          href="https://calendly.com/incrementtech2020/services-offered"
+          target="_blank"
+          ><button>Contact Us</button></a
+        >
       </div>
     </div>
   </div>
@@ -19,6 +23,7 @@
 @import "~assets/style/colors.scss";
 button {
   margin-top: 5%;
+  margin-bottom: 10%;
   background-color: $secondary;
   color: white;
   box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.25);
@@ -39,8 +44,20 @@ button {
 }
 h3 {
   font-size: 20px;
-  margin-bottom: 5%;
+  margin-bottom: 3%;
   color: white;
+  line-height: 2;
+  width: 65%;
+  text-align: justify;
+}
+@media only screen and (max-width: 1024px) {
+  h3 {
+  font-size: 20px;
+  margin-bottom: 3%;
+  color: white;
+  line-height: 2;
+  width: 90%;
+}
 }
 .bg-primary {
   background-image: url("../../assets/img/IncrementBanner.jpg");
@@ -48,135 +65,140 @@ h3 {
 .cw-banner {
   width: 100%;
   float: left;
-  height: 100vh;
+  min-height: 100vh;
 }
 .title {
   margin-top: 10%;
-  margin-bottom: 10%;
+  margin-bottom: 5%;
   color: white;
   font-size: 40px;
-  font-weight: 700
+  font-weight: 700;
+  line-height: 1.6;
 }
 .incre-row {
   height: 900px;
 }
 @media only screen and (max-width: 1024px) {
-.item-holder {
-  top: 55%;
-  width: 70%;
-}
-.cw-banner {
-  height: 75vh;
-}
+  .item-holder {
+    top: 55%;
+    width: 70%;
+  }
+  .cw-banner {
+    height: 75vh;
+  }
 }
 @media only screen and (max-width: 540px) {
-button {
-  padding: 10px 30px;
-  font-size: 20px;
-  margin-bottom: 15%;
-}
-.item-holder {
-  top: 40%;
-  width: 70%;
-}
-h3{
-  font-size: 16px;
-}
-.title {
-  font-size: 35px;
-  font-weight: 600
-}
+  button {
+    padding: 10px 30px;
+    font-size: 20px;
+    margin-bottom: 15%;
+  }
+  .item-holder {
+    top: 40%;
+    width: 70%;
+  }
+  h3 {
+    font-size: 16px;
+  }
+  .title {
+    font-size: 35px;
+    font-weight: 600;
+  }
 }
 @media only screen and (max-width: 485px) {
-.item-holder {
-  top: 30%;
-  width: 70%;
-}
-h3{
-  font-size: 16px;
-}
-.title {
-  font-size: 35px;
-  font-weight: 600
-}
+  .item-holder {
+    top: 30%;
+    width: 70%;
+  }
+  h3 {
+    font-size: 16px;
+    width: 95%;
+    margin-top: 5vh;
+    margin-bottom: 5vh;
+    text-align: justify;
+  }
+  .title {
+    font-size: 35px;
+    font-weight: 600;
+  }
 }
 @media only screen and (max-width: 414px) {
-.item-holder {
-  top: 40%;
-  width: 70%;
-}
-h3{
-  font-size: 16px;
-}
-.title {
-  font-size: 35px;
-  font-weight: 600
-}
+  .item-holder {
+    top: 40%;
+    width: 70%;
+  }
+  h3 {
+    font-size: 16px;
+  }
+  .title {
+    font-size: 35px;
+    font-weight: 600;
+  }
 }
 @media only screen and (max-width: 411px) {
-.item-holder {
-  top: 45%;
-  width: 70%;
-}
-.title {
-  font-size: 35px;
-  font-weight: 600
-}
+  .item-holder {
+    top: 45%;
+    width: 70%;
+  }
+  .title {
+    font-size: 35px;
+    font-weight: 600;
+  }
 }
 @media only screen and (max-width: 375px) {
-.item-holder {
-  top: 50%;
-  width: 70%;
-}
-h3{
-  font-size: 16px;
-}
-.title{
-  font-size: 30px;
-}
+  .item-holder {
+    top: 50%;
+    width: 70%;
+  }
+  h3 {
+    font-size: 16px;
+  }
+  .title {
+    font-size: 30px;
+  }
 }
 @media only screen and (max-width: 360px) {
-.title {
-  font-size: 30px;
-}
-.item-holder {
-  width: 70%;
-}
-h3{
-  font-size: 16px;
-}
+  .title {
+    font-size: 30px;
+  }
+  .item-holder {
+    width: 70%;
+  }
+  h3 {
+    font-size: 16px;
+  }
 }
 @media only screen and (max-width: 320px) {
-.item-holder {
-  top: 45%;
-  width: 75%;
-}
-.title{
-  font-size: 25px;
-}
-h3{
-  font-size: 16px;
-}
-button {
-  padding: 10px 20px;
-  font-size: 15px;
-  margin-bottom: 15%;
-}
+  .item-holder {
+    top: 45%;
+    width: 75%;
+  }
+  .title {
+    font-size: 25px;
+  }
+  h3 {
+    font-size: 16px;
+  }
+  button {
+    padding: 10px 20px;
+    font-size: 15px;
+    margin-bottom: 15%;
+  }
 }
 @media only screen and (max-width: 280px) {
-.title {
-  font-size: 25px;
-  margin-bottom: 15%;
-}
-h3 {
-  font-size: 15px;
-}
-button {
-  padding: 5px 20px;
-  font-size: 20px;
-  margin-bottom: 15%;
-  font-size: 15px;
-}
+  .title {
+    font-size: 25px;
+    margin-bottom: 15%;
+  }
+  h3 {
+    font-size: 15px;
+  }
+  button {
+    padding: 5px 20px;
+    font-size: 20px;
+    margin-bottom: 15%;
+    font-size: 15px;
+  }
 }
 </style>
 <script>
