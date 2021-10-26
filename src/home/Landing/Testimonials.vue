da2d6027488c85615d88f86d1f9887432aca759c
LEulin <nylael.nilue18181919@gmail.com>
2021-10-26 17:29:06 +0800

    updates

--- src/home/Landing/Testimonials.vue
+++ src/home/Landing/Testimonials.vue
@@ -1,32 +1,25 @@
 <template>
   <div>
-    <div class="curve">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#8f00b4"
-          fill-opacity="1"
-          d="M0 114.359H1919V72.1293C1919 72.1293 1818.31 6.27769e-05 1627.8 0C1437.3 -6.27764e-05 1257.73 110.599 1072.05 110.599C999.157 110.599 927.457 94.1749 858.089 72.9421C765.056 44.4665 654.178 0 557.849 0C461.521 0 409.984 67.5828 308.769 72.9421C185.042 79.4935 0 0 0 0V114.359Z"
-        ></path>
-      </svg>
-    </div>
     <div class="banner" id="testimonials">
       <div>
         <SectionHeader
-          :title="'What They Say About Us'"
+          :title="'WHAT THEY SAY ABOUT US'"
           :style="style"
-          :description="'For the past years and until now, Increment Technologies Inc. has been serving clients around the world.'"
+          :description="'Our clients satisfactions expresses their testimonies.'"
         ></SectionHeader>
       </div>
       <div class="container">
         <div class="row no-gutters">
-          <span class="col-md-6" v-for="(item, index) in list" :key="index">
+          <span class="col-sm" v-for="(item, index) in list" :key="index">
             <center class="single-card">
               <h4 class="qoute">{{ item.quote }}</h4>
-              <h4 class="name text-white">
+              <h4 class="name">
                 <b class="name">{{ item.name }}</b>
               </h4>
-              <p>{{ item.position }}</p>
               <p>
+                {{ item.position }}
+                <br />
+                <!-- <p> -->
                 <b>{{ item.country }}</b>
               </p>
             </center>
@@ -39,30 +32,29 @@
 <style scoped lang="scss">
 @import "~assets/style/colors.scss";
 .banner {
-  min-height: 70vh;
   float: left;
   width: 100%;
-  background: linear-gradient(180deg, rgba(143, 0, 181, 0.99) 0%, #00b89f 100%);
-  margin-top: -14.5%;
+  background: linear-gradient(
+    180deg,
+    rgba(143, 0, 181, 0.99) 0%,
+    $secondary 100%
+  );
+  padding: 80px 0 80px 0;
 }
-
 .title {
   font-weight: 800;
   font-size: 50px;
 }
-
 .row {
-  margin-top: 8vh;
+  margin-top: 100px;
   text-align: center;
 }
 .qoute {
-  font-weight: 700;
   font-style: italic;
-  height: 150px;
 }
-
 .name {
   color: $primary;
+  margin-top: 100px;
 }
 
 .single-card {
@@ -75,25 +67,16 @@
   border-radius: 10px;
 }
 .container {
-  padding-top: 10vh;
-  padding-bottom: 10vh;
   text-align: center;
   color: white;
 }
-.curve {
-  width: 100%;
-  float: left;
-  background-color: $white;
-}
-.pText {
-  margin-top: 3%;
-  font-size: 16px;
-  line-height: 22px;
-  text-align: center;
-}
-.bottom svg {
-  display: block;
-  margin-top: -340px;
+@media only screen and (max-width: 992px) {
+  .single-card {
+    margin-top: 20px;
+  }
+  .row{
+    margin-top: 50px;
+  }
 }
 </style>
 <script>
@@ -108,6 +91,7 @@ export default {
       style: {
         color: ColorsJS.white,
       },
+      curveStyle: ColorsJS.purple,
       list: [
         {
           quote:
--- src/home/generic/sectionHeader.vue
+++ src/home/generic/sectionHeader.vue
@@ -1,32 +1,34 @@
 <template>
-    <div class="title-holder">
-        <h2 class="title" :style="stylecss"><b>{{title}}</b></h2>
-        <p class="decription">{{description}}</p>
-    </div>
+  <div class="title-holder">
+    <h2 class="title" :style="stylecss">
+      <b>{{ title }}</b>
+    </h2>
+    <p class="decription">{{ description }}</p>
+  </div>
 </template>
 
 <style scoped lang="scss">
-@import "~assets/style/colors.scss";
-.title-holder{
-    width: 100%;
-    min-height: 50%;
-}
-
-.title{
-    font-weight: 800;
-    text-align: center;
-}
-
 .decription{
-    text-align: center;
+  margin-top: 20px
+}
+@import "~assets/style/colors.scss";
+.title-holder {
+  width: 100%;
+  min-height: 50%;
+}
+
+.title {
+  font-weight: 800;
+  text-align: center;
+}
+
+.decription {
+  text-align: center;
 }
 </style>
 
 <script>
 export default {
-    props: ['title', 'description', 'stylecss'],
-    methods:{
-
-    }
-}
+  props: ["title", "description", "stylecss"],
+};
 </script>
