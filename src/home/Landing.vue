bbce76d41a16950b49efe6130eb90fdcb3472d35
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-11-09 14:31:44 +0800

    remove returnToTop

--- src/home/Landing.vue
+++ src/home/Landing.vue
@@ -9,43 +9,14 @@
     <LandingOurValues />
     <LandingTechnologies />
     <LandingWhatTheySayAboutUs />
-    <!-- Calendly inline widget begin -->
-    <!-- <div class="calendly-inline-widget" data-url="https://calendly.com/kennettecanales/30min" style="min-width:320px;height:630px;"></div> -->
-    <!-- Calendly inline widget end -->
-    <span class="return-to-top" @click="scrollTo()">
-      <i class="fa fa-angle-up" style="font-size: 35px"></i>
-    </span>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "~assets/style/colors.scss";
-
-.return-to-top {
-  position: fixed;
-  bottom: 10px;
-  left: 10px;
-  background: $primary;
-  color: $white;
-  height: 50px;
-  width: 50px;
-  border-radius: 5px;
-  line-height: 62px;
-  text-align: center;
-}
-
-.fa-angle-up {
-  color: $white !important;
-}
-.return-to-top:hover {
-  cursor: pointer;
-  color: $primary;
-}
 </style>
 
 <script>
-import ROUTER from "src/router";
-import Jquery from "jquery";
 import LandingBanner from "src/home/Landing/Banner.vue";
 import LandingWhoWeAre from "src/home/Landing/WhoWeAre.vue";
 import LandingOurServices from "src/home/Landing/Services.vue";
@@ -66,23 +37,6 @@ export default {
     LandingOurValues,
     LandingTechnologies,
     LandingWhatTheySayAboutUs,
-  },
-  mounted() {},
-  data() {
-    return {};
-  },
-  methods: {
-    redirect(parameter) {
-      ROUTER.push(parameter);
-    },
-    scrollTo() {
-      Jquery("html, body").animate(
-        {
-          scrollTop: Jquery(".body").offset().top,
-        },
-        500
-      );
-    },
   },
 };
 </script>
